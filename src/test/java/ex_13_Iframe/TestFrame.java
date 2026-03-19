e1853b4ea30781b286b37d06179e6c367997b0a8
Ramya-sdet <ramyasdet0@gmail.com>
2026-03-19 15:46:20 +0530

    Iframes

---
+++ src/test/java/ex_13_Iframe/TestFrame.java
@@ -0,0 +1,22 @@
+package ex_13_Iframe;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestFrame {
+    @Test
+    public void test_frame() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://codepen.io/AbdullahSajjad/full/LYGVRgK");
+
+        driver.switchTo().frame("result");
+        driver.findElement(By.id("username")).sendKeys("Div");
+
+
+        driver.switchTo().defaultContent();
+
+    }
+}
---
+++ src/test/java/ex_13_Iframe/TestFrame2.java
@@ -0,0 +1,24 @@
+package ex_13_Iframe;
+
+import org.example.CommonToAll;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestFrame2  extends CommonToAll {
+    @Test
+    public void test_frame(){
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://codepen.io/AbdullahSajjad/full/LYGVRgK");
+
+        IFrameUtils frameUtils = new IFrameUtils(driver);
+
+        frameUtils.switchToFrame("result");
+        driver.findElement(By.id("username")).sendKeys("Shree");
+        frameUtils.switchToDefault();
+
+
+    }
+}
